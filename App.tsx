738ee2e4f5ae9d629b478c38b45f66d495a9d897
Jr Bytes <itamazor@gmail.com>
2022-11-01 22:56:01 -0300

    configured statusbar to use dark theme

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider } from "native-base"
+import { NativeBaseProvider, StatusBar } from "native-base"
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { SignIn } from './src/screens/SignIn'
@@ -15,6 +15,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       { !fontsLoaded ? <Loading /> : <SignIn /> }
     </NativeBaseProvider>
   )
