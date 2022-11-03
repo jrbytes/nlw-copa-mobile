a544c5b4b2735c8867d240ecda88293414795b56
Jr Bytes <itamazor@gmail.com>
2022-11-03 12:56:31 -0300

    added button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,23 @@
-import { Center } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
+import { AntDesign } from '@expo/vector-icons'
+
+import { Button } from '../components/Button'
 
 import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR NO GOOGLE"
+        leftIcon={<Icon as={AntDesign} name="google" color="white" size="md" />}
+        type="secondary"
+        mt={12}
+      />
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação do Google,{'\n'} apenas para autenticação.
+      </Text>
     </Center>
   )
 }
