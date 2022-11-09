ea048b4f4da9a8b83e3f9f3f3d5bfc8c1431bdd4
Jr Bytes <itamazor@gmail.com>
2022-11-09 19:23:26 -0300

    updated new screen to create a pool

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,12 +1,56 @@
-import { Heading, VStack, Text } from "native-base"
+import { useState } from 'react'
+import { Heading, VStack, Text, useToast } from "native-base"
 
 import { Header } from "../components/Header"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
 import Logo from '../assets/logo.svg'
+import { api } from '../services/api'
 
 export function New() {
+  const [title, setTitle] = useState('')
+  const [isLoadingPoolCreate, setIsLoadingPoolCreate] = useState(false)
+
+  const toast = useToast() 
+
+  async function handlePoolCreate() {
+    const titleTrimmed = title.trim()
+    if (!titleTrimmed) {
+      return toast.show({
+        title: 'Novo bolão!',
+        description: 'Informe um nome para o seu bolão.',
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    }
+    
+    try {
+      setIsLoadingPoolCreate(true)
+
+      await api.post('/pools', { title })
+
+      toast.show({
+        title: 'Bolão criado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500',
+      })
+
+      setTitle('')
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Error!',
+        description: 'Não foi possível criar o bolão.',
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setIsLoadingPoolCreate(false)
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -22,9 +66,15 @@ export function New() {
         <Input
           mb={2}
           placeholder="Qual nome do seu bolão?"
+          onChangeText={setTitle}
+          value={title}
         />
 
-        <Button title="CRIAR MEU BOLÃO" />
+        <Button
+          title="CRIAR MEU BOLÃO"
+          onPress={handlePoolCreate}
+          isLoading={isLoadingPoolCreate}
+        />
 
         <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único
