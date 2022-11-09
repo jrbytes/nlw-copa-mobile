207cf45b7d3f2381fc0c94d080a4e34a4897bcdd
Jr Bytes <itamazor@gmail.com>
2022-11-09 20:46:54 -0300

    updated polls screen to list polls

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,45 @@
-import { VStack, Icon } from 'native-base'
+import { useCallback, useState } from 'react'
+import { VStack, Icon, useToast, FlatList } from 'native-base'
 import { Octicons } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+
+import { api } from '../services/api'
 
 import { Button } from '../components/Button'
 import { Header } from '../components/Header'
+import { Loading } from '../components/Loading'
+import { PoolCard, PoolPros } from '../components/PoolCard'
+import { EmptyPoolList } from '../components/EmptyPoolList'
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [pools, setPools] = useState<PoolPros[]>([])
   const { navigate } = useNavigation()
+  const toast = useToast()
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true)
+
+      const { data } = await api.get('/pools')
+      setPools(data.pools)
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Error!',
+        description: 'Não foi possível carregar os bolões.',
+        placement: 'top',
+        bgColor: 'red.500',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchPools()
+  }, []))
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -19,6 +52,20 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {!isLoading ? (
+        <FlatList
+          data={pools}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 10 }}
+          px={5}
+        /> 
+      ) : (
+        <Loading />
+      )}
     </VStack>
   )
 }
